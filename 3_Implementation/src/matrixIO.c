623b4a1806928039eb351f012c20f41846d35fea
Hrushikar <hrushikarteja.k@gmail.com>
2021-04-15 09:53:39 +0530

    Removed warnings.

--- 3_Implementation/src/matrixIO.c
+++ 3_Implementation/src/matrixIO.c
@@ -7,13 +7,13 @@ void matrixInputInteger(long int** mat1, long int** mat2, int r1, int c1, int r2
         printf("\nEnter the elements of the first matrix:\n");
         for (int i = 0; i < r1; i++){
             for (int j = 0; j < c1; j++){
-                scanf("%d", &mat1[i][j]);
+                scanf("%ld", &mat1[i][j]);
             }
         }
         printf("\nEnter the elements of the second matrix:\n");
         for (int i = 0; i < r1; i++){
             for (int j = 0; j < c1; j++){
-                scanf("%d", &mat2[i][j]);
+                scanf("%ld", &mat2[i][j]);
             }
         }
         printf("\n");
@@ -22,13 +22,13 @@ void matrixInputInteger(long int** mat1, long int** mat2, int r1, int c1, int r2
         printf("\nEnter the elements of the first matrix:\n");
         for(int i=0; i<r1; i++){
             for(int j=0; j<c1; j++){
-                scanf("%d", &mat1[i][j]);
+                scanf("%ld", &mat1[i][j]);
             }
         }
         printf("\nEnter the elements of the second matrix:\n");
         for(int i=0; i<r2; i++){
             for(int j=0; j<c2; j++){
-                scanf("%d", &mat2[i][j]);
+                scanf("%ld", &mat2[i][j]);
             }
         }
     }
@@ -73,7 +73,7 @@ void matrixInputDouble(double** mat1, double** mat2, int r1, int c1, int r2, int
 void matrixPrintInteger(long int** mat, int r, int c){
     for(int i=0; i<r; i++){
         for(int j=0; j<c; j++){
-            printf("%d", mat[i][j]);
+            printf("%ld", mat[i][j]);
         }
         printf("\n");
     }
