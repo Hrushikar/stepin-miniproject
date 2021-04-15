a43a20d4bd5d47bb4d2cd076d19d4f76dec784ed
Hrushikar <hrushikarteja.k@gmail.com>
2021-04-15 09:29:53 +0530

    Removed undefined reference.

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -33,37 +33,10 @@
 // void multiplyDouble(void);
 // void divide(void);
 
-
-// int main(){
-//     printf("********Calculator********\n\n");
-//     int a, b, answer, choice;
-//     while(1)
-//     {
-//         printf("To add complex numbers press 1.\n");
-//         printf("To subtract complex numbers press 2.\n");
-//         printf("To multiply complex numbers press 3.\n");
-//         printf("To divide complex numbers press 4.\n");
-//         printf("To exit press 5.\n");
-//         printf("Enter the number.\n");
-//         scanf("%d", &choice);
-//         if(choice == 1){
-//             error_t res = complex_sum(&a,&b,&answer);
-//         }
-//         if(choice == 2){
-//             error_t res = complex_diff(&a,&b,&answer);
-//                     }
-//         if(choice == 3){
-//             error_t res = complex_mul(&a,&b,&answer);
-//         }
-//         if(choice == 4){
-//             error_t res = complex_div(&a,&b,&answer);
-//         }  
-//         if(choice == 5){
-//             break;
-//         }
-//     }
-//     return 0;
-// }
+void matrixInputInteger(long int** mat1, long int** mat2, int r1, int c1, int r2, int c2);
+void matrixInputDouble(double** matd1, double** matd2, int r1, int c1, int r2, int c2);
+void matrixPrintInteger(long int** res_mat, int r3, int c3);
+void matrixPrintDouble(double** res_matd, int r3, int c3);
 
 
 int main(){
